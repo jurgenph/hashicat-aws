ff1190bd67df8c24153a5a10bab69960685da46c
Jurgen Philippaerts <jurgen.philippaerts@earnin.com>
2020-02-28 14:35:56 -0800

    vpc for real

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,19 @@
 module "vpc" {
   source  = "app.terraform.io/jurgen-training/vpc/aws"
   version = "2.24.0"
-  cidr+block = "10.0.0.0/21"
-  database_subnet_assign_ipv6_address_on_creation = "false"
-  elasticache_subnet_assign_ipv6_address_on_creation = "false"
-  enable_classiclink = "false"
-  enable_classiclink_dns_support = "false"
-  intra_subnet_assign_ipv6_address_on_creation = "false"
-  private_subnet_assign_ipv6_address_on_creation = "false"
-  public_subnet_assign_ipv6_address_on_creation = "false"
-  redshift_subnet_assign_ipv6_address_on_creation = "false"
+
+  name = "this-is-not-a-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1a", "us-east-1a"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
